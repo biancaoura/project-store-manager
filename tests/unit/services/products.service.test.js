4325bf0559132b8dc45a2fcf2eb0ffc333b5d137
Bianca Oura <biancaoura@gmail.com>
2022-11-13 16:34:52 -0300

    test(products): service: add /post

--- tests/unit/services/products.service.test.js
+++ tests/unit/services/products.service.test.js
@@ -10,27 +10,53 @@ describe('Unit tests (Service) - Products', function () {
 
   afterEach(sinon.restore);
 
-  it('1 - Should list all products', async function () {
-    sinon.stub(productsModel, 'getAllProducts').resolves(allProducts);
+  describe('Listing products', function () {
+    it('1 - Should list all products', async function () {
+      sinon.stub(productsModel, 'getAllProducts').resolves(allProducts);
+      
+      const { message } = await productsService.getAllProducts();
+      
+      expect(message).to.deep.equal(allProducts);
+    });
 
-    const { message } = await productsService.getAllProducts();
-
-    expect(message).to.deep.equal(allProducts);
+    it('2 - Should get matching product when searching by id', async function () {
+      sinon.stub(productsModel, 'getProductById').resolves(allProducts[0]);
+      
+      const { message } = await productsService.getProductById(1);
+      
+      expect(message).to.deep.equal(allProducts[0]);
+    });
+    
+    it('3 - Should return an error message if id is not found', async function () {
+      sinon.stub(productsModel, 'getProductById').returns(undefined);
+      
+      const { message } = await productsService.getProductById(10);
+      
+      expect(message).to.deep.equal('Product not found');
+    });
   });
 
-  it('2 - Should get matching product when searching by id', async function () {
-    sinon.stub(productsModel, 'getProductById').resolves(allProducts[0]);
+  describe('Creating products', function () {
+    it('1 - Should throw an error if no name is given', async function () {
+      const { type, message } = await productsService.createProduct();
 
-    const { message } = await productsService.getProductById(1);
+      expect(type).to.equal(400);
+      expect(message).to.equal('"name" is required');
+    });
 
-    expect(message).to.deep.equal(allProducts[0]);
-  });
+    it('2 - Should throw an error if the name has less than 5 characters', async function () {
+      const { type, message } = await productsService.createProduct('a');
 
-  it('3 - Should return an error message if id is not found', async function () {
-    sinon.stub(productsModel, 'getProductById').returns(undefined);
+      expect(type).to.equal(422);
+      expect(message).to.equal('"name" length must be at least 5 characters long');
+    });
 
-    const { message } = await productsService.getProductById(10);
+    it('3 - Should return the new product', async function () {
+      sinon.stub(productsModel, 'createProduct').resolves(10);
+      const { type, message } = await productsService.createProduct('product');
 
-    expect(message).to.deep.equal('Product not found');
+      expect(type).to.equal(null);
+      expect(message).to.deep.equal({ id: 10, name: 'product' });
+    });
   });
 });
