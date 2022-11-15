21fe67a4424512e4912decedf7f6bc46d096db33
Bianca Oura <biancaoura@gmail.com>
2022-11-14 22:47:00 -0300

    feat(sales): route: /get sale by id

--- src/routes/sales.route.js
+++ src/routes/sales.route.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const { salesController } = require('../controllers');
 
 router.get('/', salesController.getAllSales);
+router.get('/:id', salesController.getSaleById);
 router.post('/', salesController.createSale);
 
 module.exports = router;
