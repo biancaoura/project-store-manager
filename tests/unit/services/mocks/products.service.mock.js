9a708c4c02331aade9672af2fa583847c7d7fea4
Bianca Oura <biancaoura@gmail.com>
2022-11-15 01:32:32 -0300

    test(products): service: add (update) tests and mocks

---
+++ tests/unit/services/mocks/products.service.mock.js
@@ -0,0 +1,11 @@
+const newProduct = 'Manopla de Thanos';
+
+const validNewName = { name: 'Cetro de Loki' };
+
+const invalidNameLen = { name: 'abc' };
+
+module.exports = {
+  newProduct,
+  validNewName,
+  invalidNameLen,
+};
--- tests/unit/services/products.service.test.js
+++ tests/unit/services/products.service.test.js
@@ -4,9 +4,12 @@ const { afterEach } = require('mocha');
 
 const { productsModel } = require('../../../src/models');
 const { productsService } = require('../../../src/services');
-const { allProducts } = require('../models/mocks/products.model.mock');
+const { allProducts, updatedProduct } = require('../models/mocks/products.model.mock');
+const { validNewName, invalidNameLen, newProduct } = require('./mocks/products.service.mock');
 const { createdProduct } = require('../controllers/mocks/products.controller.mock');
-const { HTTP_BAD_REQUEST, HTTP_UNPROCESSABLE_ENTITY } = require('../../../src/utils/httpStatus');
+const { HTTP_BAD_REQUEST, HTTP_UNPROCESSABLE_ENTITY, HTTP_NOT_FOUND } = require('../../../src/utils/httpStatus');
+
+const NOT_FOUND = 'Product not found';
 
 describe('Unit tests (Service) - Products', function () {
 
@@ -34,7 +37,7 @@ describe('Unit tests (Service) - Products', function () {
       
       const { message } = await productsService.getProductById(10);
       
-      expect(message).to.deep.equal('Product not found');
+      expect(message).to.deep.equal(NOT_FOUND);
     });
   });
 
@@ -57,10 +60,44 @@ describe('Unit tests (Service) - Products', function () {
       sinon.stub(productsModel, 'createProduct').resolves([{ insertId: 10 }]);
       sinon.stub(productsModel, 'getProductById').resolves(createdProduct);
 
-      const { type, message } = await productsService.createProduct('Manopla de Thanos');
+      const { type, message } = await productsService.createProduct(newProduct);
 
       expect(type).to.equal(null);
       expect(message).to.deep.equal(createdProduct);
+    });
+  });
+
+  describe('Updating products', function () {
+    it('1 - Should throw an error if product id doesn\'t exist', async function () {
+      sinon.stub(productsModel, 'updateProduct').returns(undefined);
+
+      const { type, message } = await productsService.updateProduct(10, validNewName);
+
+      expect(type).to.equal(HTTP_NOT_FOUND);
+      expect(message).to.equal(NOT_FOUND);
+    });
+
+    it('2 - Should throw an error if no new name is given', async function () {
+      const { type, message } = await productsService.updateProduct(1, {});
+
+      expect(type).to.equal(HTTP_BAD_REQUEST);
+      expect(message).to.equal('"name" is required');
+    });
+
+    it('3 - Should throw an error if the new name has less than 5 characters', async function () {
+      const { type, message } = await productsService.updateProduct(1, invalidNameLen);
+
+      expect(type).to.equal(HTTP_UNPROCESSABLE_ENTITY);
+      expect(message).to.equal('"name" length must be at least 5 characters long');
+    });
+
+    it('4 - Should return the updated product info', async function () {
+      sinon.stub(productsModel, 'updateProduct').returns(updatedProduct);
+
+      const { type, message } = await productsService.updateProduct(1, validNewName);
+
+      expect(type).to.equal(null);
+      expect(message).to.deep.equal(updatedProduct);
     });
   });
 });
