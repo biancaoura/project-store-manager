61fb3a106351da04f1b43b7fee02a32bbba6dee9
Bianca Oura <biancaoura@gmail.com>
2022-11-15 00:32:04 -0300

    feat(products): route: post /:id

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -6,6 +6,9 @@ const { productsController } = require('../controllers');
 
 router.get('/', productsController.getAllProducts);
 router.get('/:id', productsController.getProductById);
+
 router.post('/', productsController.createProduct);
+
+router.put('/:id', productsController.updateProduct);
 
 module.exports = router;
