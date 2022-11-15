6bed0d51af46aaf89cd5d60b3a932ce5ced54142
Bianca Oura <biancaoura@gmail.com>
2022-11-15 03:02:27 -0300

    feat(sales): route: delete: /:id

--- src/routes/sales.route.js
+++ src/routes/sales.route.js
@@ -6,6 +6,9 @@ const { salesController } = require('../controllers');
 
 router.get('/', salesController.getAllSales);
 router.get('/:id', salesController.getSaleById);
+
 router.post('/', salesController.createSale);
+
+router.delete('/:id', salesController.deleteSale);
 
 module.exports = router;
